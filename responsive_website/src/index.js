a3fc7dc2cacc2825ed48d86cba06ef582ef4121c
Sivils2018 <98772381+Sivils2018@users.noreply.github.com>
2023-05-23 19:59:53 -0400

    Add files via upload

---
+++ responsive_website/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+//This app component is going to render inside of the div on the index.html file
+ReactDOM.render(<App />, document.getElementById('root'));
+
